b0e98a5bb0ccec9cfb6f6f3b9493b73117183213
Jeffrey France <jbfrance1626@gmail.com>
2020-06-28 19:58:52 +0800

    changed stat item display

--- src/components/player/TrackStatItem.js
+++ src/components/player/TrackStatItem.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 const TrackStatItem = ({ item }) => {
   return (
     <div className='trackstat-item' style={{ background: item.bg }}>
-      <p className='value'>
-        {Array.isArray(item.data)
-          ? item.data.map((i) => {
-              return <span key={i.id}>{i.name}</span>;
-            })
-          : item.data}
+      <p className='value' style={{ textAlign: 'center' }}>
+        {Array.isArray(item.data) ? (
+          <span key={item.data[0].id}>{item.data[0].name}</span>
+        ) : (
+          item.data
+        )}
       </p>
       <h5 className='subheading'>{item.subheading}</h5>
     </div>
